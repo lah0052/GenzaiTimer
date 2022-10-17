b18b45a489603183e65748db14413a366d79ba48
lah0052 <lah0052@mix.wvu.edu>
2022-10-17 19:16:14 -0400

    routing

--- GenzaiTimer/src/app/app-routing.module.ts
+++ GenzaiTimer/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { JournalLayoutComponent } from './journal-layout/journal-layout.component';
+import { PlannerLayoutComponent } from './planner-layout/planner-layout.component';
+import { SettingsLayoutComponent } from './settings-layout/settings-layout.component';
+
 
 const routes: Routes = [
   {path: "", component: HomeLayoutComponent},
-  {path: "home", component: HomeLayoutComponent}
+  {path: "home", component: HomeLayoutComponent},
+  {path: "planner", component: PlannerLayoutComponent},
+  {path: "journal", component: JournalLayoutComponent},
+  {path: "settings", component: SettingsLayoutComponent}
 ];
 
 @NgModule({
