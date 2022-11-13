8124729dd50b0e3b04a583a5f79d70d7fe0731b7
lah0052 <lah0052@mix.wvu.edu>
2022-11-13 07:28:43 -0500

    calender

--- GenzaiTimer/src/app/planner-components/calender.component.js
+++ GenzaiTimer/src/app/planner-components/calender.component.js
@@ -1,83 +1,42 @@
-// generate events
-var eventDates = {}
-let day1 = formatDate(new Date(new Date().setMonth(new Date().getMonth() + 1)))
-eventDates[day1] = [
-  'Event 1, Location',
-  'Event 2, Location 2'
-]
-let day2 = formatDate(new Date(new Date().setDate(new Date().getDate() + 40)))
-eventDates[day2] = [
-  'Event 2, Location 3',
-]
 
-// set maxDates
-var maxDate = {
-  1: new Date(new Date().setMonth(new Date().getMonth() + 11)),
-  2: new Date(new Date().setMonth(new Date().getMonth() + 10)),
-  3: new Date(new Date().setMonth(new Date().getMonth() + 9))
-}
 
-var flatpickr = $('#calendar .placeholder').flatpickr({
-  inline: true,
-  minDate: 'today',
-  maxDate: maxDate[3]
-,
-  showMonths: 1,
-  enable: Object.keys(eventDates),
-  disableMobile: "true",
-  onChange: function(date, str, inst) {
-    var contents = '';
-    if(date.length) {
-        for(i=0; i < eventDates[str].length; i++) {
-        contents += '<div class="event"><div class="date">' + flatpickr.formatDate(date[0], 'l J F') + '</div><div class="location">' + eventDates[str][i] + '</div></div>';
-      }
-    }
-    $('#calendar .calendar-events').html(contents)
-  },
-  locale: {
-    weekdays: {
-      shorthand: ["S", "M", "T", "W", "T", "F", "S"],
-      longhand: [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ]
+var app = {
+    settings: {
+      container: $('.calendar'),
+      calendar: $('.front'),
+      days: $('.weeks span'),
+      form: $('.back'),
+      input: $('.back input'),
+      buttons: $('.back button')
+    },
+  
+    init: function() {
+      instance = this;
+      settings = this.settings;
+      this.bindUIActions();
+    },
+  
+    swap: function(currentSide, desiredSide) {
+      settings.container.toggleClass('flip');
+  
+      currentSide.fadeOut(900);
+      currentSide.hide();
+      desiredSide.show();
+  
+    },
+  
+    bindUIActions: function() {
+      settings.days.on('click', function(){
+        instance.swap(settings.calendar, settings.form);
+        settings.input.focus();
+      });
+  
+      settings.buttons.on('click', function(){
+        instance.swap(settings.form, settings.calendar);
+      });
     }
   }
-})
-
-eventCaledarResize($(window));
-$(window).on('resize', function() {
-  eventCaledarResize($(this))
-})
-
-function eventCaledarResize($el) {
-  var width = $el.width()
-  if(flatpickr.selectedDates.length) {
-    flatpickr.clear()
-  }
-  if(width >= 992 && flatpickr.config.showMonths !== 3) {
-    flatpickr.set('showMonths', 3)
-    flatpickr.set('maxDate', maxDate[3])
-  }
-  if(width < 992 && width >= 768 && flatpickr.config.showMonths !== 2) {
-    flatpickr.set('showMonths', 2)
-    flatpickr.set('maxDate', maxDate[2])
-  }
-  if(width < 768 && flatpickr.config.showMonths !== 1) {
-    flatpickr.set('showMonths', 1)
-    flatpickr.set('maxDate', maxDate[1])
-    $('.flatpickr-calendar').css('width', '')
-  }
-}
-
-function formatDate(date) {
-    let d = date.getDate();
-    let m = date.getMonth() + 1; //Month from 0 to 11
-    let y = date.getFullYear();
-    return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
-}
+  
+  app.init();
+  
+  
