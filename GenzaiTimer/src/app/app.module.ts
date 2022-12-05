b9d7778473030e84847fcef909862eb1573a5618
ACOGG24 <ajc0094@mix.wvu.edu>
2022-12-05 08:26:36 -0500

    added firebase login authentification

--- GenzaiTimer/src/app/app.module.ts
+++ GenzaiTimer/src/app/app.module.ts
@@ -8,10 +8,8 @@ import { JournalLayoutComponent } from './journal-layout/journal-layout.componen
 import { PlannerLayoutComponent } from './planner-layout/planner-layout.component';
 import {SideNavbarComponent} from './home-components/side-navbar/side-navbar.component';
 import {HttpClientModule} from '@angular/common/http';
-
-
+import { NgForm } from '@angular/forms';
 import { TempTimerComponent } from './home-components/temp-timer/temp-timer.component';
-
 import { TaskListComponent } from './home-components/task-list/task-list.component';
 import { NotificationComponent } from './home-components/notification/notification.component';
 import { CalenderComponent } from './planner-components/calender/calender.component';
@@ -21,6 +19,10 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AddTaskComponent } from './home-components/task-list/add-task/add-task.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,7 +42,12 @@ import { AddTaskComponent } from './home-components/task-list/add-task/add-task.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- GenzaiTimer/src/app/login-layout/login-layout.component.ts
+++ GenzaiTimer/src/app/login-layout/login-layout.component.ts
@@ -1,23 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { NgForOf } from "@angular/common";
+import { Observable } from 'rxjs';
+import { loginService } from './login.service';
+import { loginResponse } from './loginResponse';
 
 @Component({
   selector: 'app-login-layout',
   templateUrl: './login-layout.component.html',
   styleUrls: ['./login-layout.component.css']
 })
-export class LoginLayoutComponent implements OnInit {
+export class LoginLayoutComponent {
 
-  public onSubmit(data: NgForm) {
-    console.log("Button Clicked");
-    console.log(data.value);
+  public buttonClicked!:string;
+    private loginObservable!: Observable<loginResponse>;
 
-  }
+    constructor(private loginService:loginService) {
+
+    }
+
+    public onSubmit(data: NgForm) {
+        console.log("Button clicked = " + this.buttonClicked);
+        console.log(data.value);
+
+        if(this.buttonClicked == 'SignUp') {
+            this.loginObservable = this.loginService.signup(data.value.email, data.value.password);
+        }
+
+        if(this.buttonClicked == 'Login') {
+            this.loginObservable = this.loginService.login(data.value.email, data.value.password);
+        }
+
+        this.loginObservable.subscribe(
+            (data:loginResponse) => {
+                console.log(data);
+            },
+            error => {
+                console.log(error.error);
+            }
+        );
+        data.resetForm();
+
+    }
 
 
-  constructor() { }
+ 
 
-  ngOnInit(): void {
-  }
+  
 
 }
