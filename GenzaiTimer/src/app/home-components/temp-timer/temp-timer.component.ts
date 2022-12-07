5f4c6045dd8cb3074abd2e283a9145222b88b3f6
JOATjeaton <97566990+JOATjeaton@users.noreply.github.com>
2022-12-07 11:00:12 -0500

    First iteration of a working temp timer

--- GenzaiTimer/src/app/home-components/temp-timer/temp-timer.component.ts
+++ GenzaiTimer/src/app/home-components/temp-timer/temp-timer.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, Observable} from "rxjs";
+import {map, shareReplay} from "rxjs/operators";  
 
 @Component({
   selector: 'app-temp-timer',
   templateUrl: './temp-timer.component.html',
   styleUrls: ['./temp-timer.component.css']
 })
-export class TempTimerComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+export class TempTimerComponent {
+
+  constructor() { 
+    this.timeLeft$ = interval(1000).pipe(
+      map(x => calcDateDiff()),
+        shareReplay(1)
+    );
   }
-
+ 
+  public timeLeft$: Observable<timeComponents>;
 }
+
+function calcDateDiff(endDay: Date = new Date(2022, 20, 1)): 
+timeComponents {
+
+  const dDay = endDay.valueOf();
+
+  const milliSecondsInASecond = 1000;
+  const hoursInADay = 24;
+  const minutesInAnHour = 60;
+  const secondsInAMinute = 60;
+
+  const timeDifference = dDay - Date.now();
+
+  const daysToDday = Math.floor(
+    timeDifference /
+      (milliSecondsInASecond * minutesInAnHour * secondsInAMinute * hoursInADay)
+  );
+
+  const hoursToDday = Math.floor(
+    (timeDifference /
+      (milliSecondsInASecond * minutesInAnHour * secondsInAMinute)) %
+      hoursInADay
+  );
+
+  const minutesToDday = Math.floor(
+    (timeDifference / (milliSecondsInASecond * minutesInAnHour)) %
+      secondsInAMinute
+  );
+
+  const secondsToDday =
+    Math.floor(timeDifference / milliSecondsInASecond) % secondsInAMinute;
+
+  return { secondsToDday, minutesToDday, hoursToDday, daysToDday };
+}
+
+interface timeComponents {
+  secondsToDday: number;
+  minutesToDday: number;
+  hoursToDday: number;
+  daysToDday: number;
+}
+
