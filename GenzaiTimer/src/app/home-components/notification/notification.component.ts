a5df5be71b24a24e5a0789d3eba9bf4dc1e7f2e2
chrishenry123 <35176302+chrishenry123@users.noreply.github.com>
2022-12-08 18:30:31 -0500

    Sends motivation notifaction

--- GenzaiTimer/src/app/home-components/notification/notification.component.ts
+++ GenzaiTimer/src/app/home-components/notification/notification.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+let quotes = [
+  {
+    quote: "Don't give up!",
+    author: "Chris"
+  },
+  {
+    quote: "Hang in there!",
+    author: "Jordan"
+  },
+  {
+    quote: "You got this!",
+    author: "Luke"
+  },
+  {
+    quote: "Keep going!",
+    author: "Chris1"
+  },
+  {
+    quote: "You're doing great!",
+    author: "Jordan1"
+  },
+  {
+    quote: "Keep up the hard work!",
+    author: "Luke1"
+  },
+]
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
-  
-  constructor() { }
+  show = true
+  quote = "log"
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.sendQuote()
+    setInterval(() => {
+      this.sendQuote() 
+    }, 10000);
   }
-  
+
+  async sendQuote() {
+
+    let tempQuote = quotes[Math.floor(Math.random() * 5)].quote
+    while (tempQuote == this.quote) {
+      tempQuote = quotes[Math.floor(Math.random() * 5)].quote
+    }
+    this.quote = tempQuote
+    this.show = true
+    await sleep(5000)
+    this.show = false
+  }
 }
