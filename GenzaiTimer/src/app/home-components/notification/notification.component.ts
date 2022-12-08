63dcd0d67a7aae24b1b04597ba063454238b9fc3
chrishenry123 <35176302+chrishenry123@users.noreply.github.com>
2022-12-08 15:25:48 -0500

    added sliding notif

--- GenzaiTimer/src/app/home-components/notification/notification.component.ts
+++ GenzaiTimer/src/app/home-components/notification/notification.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 }
