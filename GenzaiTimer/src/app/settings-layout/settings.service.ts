5d0455e1cd43ad1f8cc9219494dc3a4bf02fd7ab
jaredtbehr <jtb0032@mix.wvu.edu>
2022-12-13 11:04:50 -0500

    ADDED BACKEND USER SUPPORT

--- GenzaiTimer/src/app/settings-layout/settings.service.ts
+++ GenzaiTimer/src/app/settings-layout/settings.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { SettingsModel } from "./settings.model";
 import { AngularFireDatabase } from '@angular/fire/compat/database'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 @Injectable(
     {providedIn: 'root'}
@@ -19,9 +20,23 @@ export class SettingsService
 
     changeSettings(settings: SettingsModel)
     {
-        this.db.database.ref("users/" + "0" + "/settings/work").set(settings.work);
-        this.db.database.ref("users/" + "0" + "/settings/interval").set(settings.interval);
-        this.db.database.ref("users/" + "0" + "/settings/shortBreak").set(settings.shortBreak);
-        this.db.database.ref("users/" + "0" + "/settings/longBreak").set(settings.longBreak);
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+        if(user)
+        {
+                const uid = user.uid;
+                this.db.database.ref("users/" + uid + "/settings/work").set(settings.work);
+                this.db.database.ref("users/" + uid + "/settings/interval").set(settings.interval);
+                this.db.database.ref("users/" + uid + "/settings/shortBreak").set(settings.shortBreak);
+                this.db.database.ref("users/" + uid + "/settings/longBreak").set(settings.longBreak);
+            }
+            else{
+                console.log("No user logged in, data pushed to default");
+                this.db.database.ref("users/" + "0" + "/settings/work").set(settings.work);
+                this.db.database.ref("users/" + "0" + "/settings/interval").set(settings.interval);
+                this.db.database.ref("users/" + "0" + "/settings/shortBreak").set(settings.shortBreak);
+                this.db.database.ref("users/" + "0" + "/settings/longBreak").set(settings.longBreak);
+            }
     }
 }
