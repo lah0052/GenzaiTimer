c43b931a71ee6e61ad6fe58db58c66884de489cb
KadenQMc <kqm0001@mix.wvu.edu>
2022-12-13 13:31:09 -0500

    clear tasks

--- GenzaiTimer/src/app/home-components/task-list/add-task/add-task.component.ts
+++ GenzaiTimer/src/app/home-components/task-list/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { NgForm } from '@angular/forms';
+import { getAuth } from 'firebase/auth';
 import { TaskListModel } from '../task-list-model.model';
 import { TaskListService } from '../task-list.service';
 
@@ -11,14 +13,33 @@ import { TaskListService } from '../task-list.service';
 })
 export class AddTaskComponent implements OnInit {
 
-  constructor(private ps:TaskListService) { }
+  buttonClicked!: string;
+
+  constructor(private ps:TaskListService, private db:AngularFireDatabase) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(data: NgForm){
-    this.ps.addTasks(data.value);
-    data.resetForm;
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if(this.buttonClicked == 'AddTask'){
+      console.log("add");
+      this.ps.addTasks(data.value);
+      data.resetForm;
+    }
+    else if(this.buttonClicked == 'ClearTasks'){
+      console.log("clear");
+      if(user){
+        this.db.database.ref("users/" + user.uid + "/TaskList").remove();
+        location.reload();
+      }
+      else{
+        this.db.database.ref("users/0/TaskList").remove();
+        location.reload();
+      }
+    }
   }
 
 }
--- GenzaiTimer/src/app/home-components/task-list/task-list.component.ts
+++ GenzaiTimer/src/app/home-components/task-list/task-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TaskListService } from './task-list.service';
 
 @Component({
   selector: 'app-task-list',
@@ -9,11 +11,17 @@ export class TaskListComponent implements OnInit {
   @Input() name: string;
   
 
-  constructor() {
+  constructor(private ps:TaskListService) {
     this.name = "No Task"
    }
 
   ngOnInit(): void {
     
+  }
+
+  onRemove(data: NgForm){
+    this.ps.addTasks(data.value);
+    data.resetForm;
+    console.log(data.value);
   }
 }
