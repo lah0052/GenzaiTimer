21ec56a9f834c07477532c96447b470ddc815499
jaredtbehr <jtb0032@mix.wvu.edu>
2022-12-14 12:00:29 -0500

    Added functionality to sign out button

--- GenzaiTimer/src/app/user-infor/user-infor.component.ts
+++ GenzaiTimer/src/app/user-infor/user-infor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { getAuth } from 'firebase/auth';
 
 @Component({
   selector: 'app-user-infor',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-infor.component.css']
 })
 export class UserInforComponent implements OnInit {
+  @Input() email: any;
 
-  constructor() { }
+  constructor() { 
+    const auth = getAuth();
+    const user = auth.currentUser;
+    
+    if(user)
+    {
+        this.email = user.email;
+    }
+    else
+    {
+        this.email = "Guest";
+    }
+  }
 
   ngOnInit(): void {
   }
