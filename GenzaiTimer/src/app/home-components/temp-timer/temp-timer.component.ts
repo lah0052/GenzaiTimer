e7cb02cfbfe4bf3650742c76afdeb66baba8c94c
JOATjeaton <jdoge10@gmail.com>
2022-12-14 17:12:42 -0500

    START BUTTON

--- GenzaiTimer/src/app/home-components/temp-timer/temp-timer.component.ts
+++ GenzaiTimer/src/app/home-components/temp-timer/temp-timer.component.ts
@@ -2,33 +2,92 @@ import { Component, OnInit } from '@angular/core';
 import {interval, Observable, TimeoutError} from "rxjs";
 import {map, shareReplay} from "rxjs/operators"; 
 
-let i = 0;
-
 @Component({
   selector: 'app-temp-timer',
   templateUrl: './temp-timer.component.html',
   styleUrls: ['./temp-timer.component.css'],
+  
 })
 
 export class TempTimerComponent {
+  secondsCountDown = 0; //incrememnts by 1000 every second (1000 miliseconds)
+  thirtyMinutesInMiliseconds = 30*60*1000;
+  fiveMinutesInMiliseconds = 5*60*1000;
+  fiveteenMinutesInMiliseconds = 15*60*1000;
+  isStopped:boolean = true;
+  current_time = this.thirtyMinutesInMiliseconds;
+
 
 
   constructor() { 
-    //let thirtyminsinmilisec = 1000*60*30;
-    var current_time = 1000*60*30;
-
-    this.timeLeft$ = interval(1000).pipe(
-      map(x => calcDateDiff(current_time)),
-        shareReplay(1)
+   // this.timeLeft$ =
+   this.timeLeft$ = interval(1000).pipe(
+      map(x => this.calcDateDiff(this.current_time))
     );
+    //timeLeft$: Observable<timeComponents>;
+    
   }
- 
   public timeLeft$: Observable<timeComponents>;
+
+  startFunction() {
+    this.isStopped = false;
+  }
+
+  calcDateDiff(time:number): 
+  timeComponents {
+  
+   // let now = new Date()
+    const milliSecondsInASecond = 1000;
+    const hoursInADay = 24;
+    const minutesInAnHour = 60;
+    const secondsInAMinute = 60;
+    //let startValue = 1000*60*30; //30 mins, 15 mins, or 5 mins
+  
+    let timeDifference =  time -this.secondsCountDown;
+    if(timeDifference < 0)
+      timeDifference = 0 
+  
+    const daysToDday = Math.floor(
+      timeDifference /
+        (milliSecondsInASecond * minutesInAnHour * secondsInAMinute * hoursInADay)
+    );
+  
+    const hoursToDday = Math.floor(
+      (timeDifference /
+        (milliSecondsInASecond * minutesInAnHour * secondsInAMinute)) %
+        hoursInADay
+    );
+  
+    const minutesToDday = Math.floor(
+      (timeDifference / (milliSecondsInASecond * minutesInAnHour)) %
+        secondsInAMinute
+    );
+  
+    const secondsToDday =
+      Math.floor(timeDifference / milliSecondsInASecond) % secondsInAMinute;
+  
+  
+    if (this.isStopped == false) {
+      this.secondsCountDown=this.secondsCountDown+1000;
+    }
+    return { secondsToDday, minutesToDday, hoursToDday, daysToDday };
+  }
+  
+
+
+
+}
+interface timeComponents {
+  secondsToDday: number;
+  minutesToDday: number;
+  hoursToDday: number;
+  daysToDday: number;
 }
 
-function calcDateDiff(time:number): 
+
+/* function calcDateDiff(time:number): 
 timeComponents {
-  i=i+1000;
+
  // let now = new Date()
   const milliSecondsInASecond = 1000;
   const hoursInADay = 24;
@@ -36,7 +95,7 @@ timeComponents {
   const secondsInAMinute = 60;
   //let startValue = 1000*60*30; //30 mins, 15 mins, or 5 mins
 
-  let timeDifference =  time -i;
+  let timeDifference =  time -secondsCountDown;
   if(timeDifference < 0)
     timeDifference = 0 
 
@@ -59,6 +118,10 @@ timeComponents {
   const secondsToDday =
     Math.floor(timeDifference / milliSecondsInASecond) % secondsInAMinute;
 
+
+  if (isStopped) {
+    secondsCountDown=secondsCountDown+1000;
+  }
   return { secondsToDday, minutesToDday, hoursToDday, daysToDday };
 }
 
@@ -68,4 +131,4 @@ interface timeComponents {
   hoursToDday: number;
   daysToDday: number;
 }
-
+*/
